e91af016cbd4d99a4250063373cd2f603afab385
Meet Joysar <meetjoysar7s@gmail.com>
2025-12-29 14:08:17 +0530

    for ex 4.2, created logger.js, middleware.sj in utils, blogs.js(Router) in controllers folde

--- index.js
+++ index.js
@@ -3,29 +3,46 @@ const express = require('express')
 const mongoose = require('mongoose')
 const Blog = require('./models/blog')
 const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
 
 const app = express()
 
-// const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(config.MONGODB_URI, { family: 4 })
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose
+    .connect(config.MONGODB_URI, { family: 4 })
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((err) => {
+        logger.error('error in connecting MongoDB:->', err.message)
+    })
 
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
+// app.get('/api/blogs', (request, response) => {
+//   Blog.find({}).then((blogs) => {
+//     response.json(blogs)
+//   })
+// })
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+// app.post('/api/blogs', (request, response) => {
+//   const blog = new Blog(request.body)
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+//   blog.save().then((result) => {
+//     response.status(201).json(result)
+//   })
+// })
 
-// const PORT = 3003
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+    logger.info(`Server running on port ${config.PORT}`)
 })
