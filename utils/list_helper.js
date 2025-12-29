a3837ed3a0789f19f2a5e8ce094edaff1bf065c7
Meet Joysar <meetjoysar7s@gmail.com>
2025-12-29 23:42:37 +0530

    Ex 4.4 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,16 @@ const dummy = (blogs) => {
     return 1
 }
 
-module.exports = {
-  dummy
+const totalLikes = (blogs) => {
+    const reducer = (sum, blog) => {
+        console.log("sum", sum)
+        console.log("blog likes", blog.likes)
+        return sum + blog.likes
+    }
+    console.log("------")
+    return blogs.length === 0
+    ? 0 
+    : blogs.reduce(reducer, 0)
 }
+
+module.exports = { dummy, totalLikes }
