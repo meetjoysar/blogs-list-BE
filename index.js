2ef21a2817f5271d6e12e5f1fad0a7d0bfebd4f2
Meet Joysar <meetjoysar7s@gmail.com>
2025-12-29 14:16:11 +0530

    for ex 4.2, created app.js(actual express app), separetd from index.js, 4.2 done finally

--- index.js
+++ index.js
@@ -1,47 +1,6 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
-const blogsRouter = require('./controllers/blogs')
-
-const app = express()
-
-logger.info('connecting to', config.MONGODB_URI)
-
-mongoose
-    .connect(config.MONGODB_URI, { family: 4 })
-    .then(() => {
-        logger.info('connected to MongoDB')
-    })
-    .catch((err) => {
-        logger.error('error in connecting MongoDB:->', err.message)
-    })
-
-app.use(express.json())
-
-// app.get('/api/blogs', (request, response) => {
-//   Blog.find({}).then((blogs) => {
-//     response.json(blogs)
-//   })
-// })
-
-// app.post('/api/blogs', (request, response) => {
-//   const blog = new Blog(request.body)
-
-//   blog.save().then((result) => {
-//     response.status(201).json(result)
-//   })
-// })
-
-app.use(middleware.requestLogger)
-
-app.use('/api/blogs', blogsRouter)
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
 
 app.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`)
