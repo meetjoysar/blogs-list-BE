4a29aa33a5ff8b825b15615a72c923ef26736ee7
Meet Joysar <meetjoysar7s@gmail.com>
2025-12-30 08:46:51 +0530

    Ex 4.5 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -5,14 +5,33 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => {
     const reducer = (sum, blog) => {
-        console.log("sum", sum)
-        console.log("blog likes", blog.likes)
+        // console.log("sum", sum)
+        // console.log("blog likes", blog.likes)
         return sum + blog.likes
     }
-    console.log("------")
+    // console.log("------")
     return blogs.length === 0
     ? 0 
     : blogs.reduce(reducer, 0)
 }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+    const reducer = (fav_blog, current_blog) => {
+        // first working way
+        // console.log('---start---')
+        // if (fav_blog.likes < current_blog.likes) {
+        //     fav_blog = current_blog
+        //     // console.log("fav_blog", fav_blog)
+        //     // console.log("curr_blog", current_blog)
+        //     return fav_blog
+        // } 
+        // return fav_blog
+        return (fav_blog.likes < current_blog.likes) ? current_blog : fav_blog
+    }
+    // console.log('---end---')
+    return blogs.length === 0
+    ? {}
+    : blogs.reduce(reducer, blogs[0])
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
